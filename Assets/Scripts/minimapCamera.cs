315d947bebcf9028acca7f7ce61eb081b2add7a7
Eonflare14 <107974034+Eonflare14@users.noreply.github.com>
2022-10-31 13:02:41 +0800

    ghastly material change fixed

--- Assets/Scripts/minimapCamera.cs
+++ Assets/Scripts/minimapCamera.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class minimapCamera : MonoBehaviour
 {
     //Serialized variables
-    [SerializeField] private float speed;
     [SerializeField] private Vector3 offset;
 
     //Private variables
@@ -17,18 +16,21 @@ public class minimapCamera : MonoBehaviour
     /// </summary>
     private void Awake()
     {
-        //Find target
+        //Find target by getting the transform of the game object tagged as "Player"
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void LateUpdate()
     {
+        //make sure a target is found
         if (target != null)
         {
-            //Calculate desired position
+            //Calculate desired position according to offset
             offsetVector = target.position + offset;
+            //if too far away from the target position
             if (Vector3.Distance(transform.position, offsetVector) > 0.1f)
             {
+                //set position to offset
                 transform.position = offsetVector;
             }
 
